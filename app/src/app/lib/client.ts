a52efa9140b37e925bfeec460b085e0ee4fd7103
Andy Kreling <krelinga@gmail.com>
2025-05-13 18:00:26 +0000

    another backend address update.

--- app/src/app/lib/client.ts
+++ app/src/app/lib/client.ts
@@ -4,7 +4,7 @@ import { createConnectTransport } from "@connectrpc/connect-web"; // Ensure you 
 
 export default function createClient() {
   const transport = createConnectTransport({
-    baseUrl: "http://ecstatic_snyder:25004", // Replace with your server URL
+    baseUrl: "http://host.docker.internal:25004", // Replace with your server URL
     // Add any additional transport options here
   });
 
