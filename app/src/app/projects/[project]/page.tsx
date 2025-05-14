4becf7ab865c5e9d4b89352d0eeca55c6db33f80
Andy Kreling <krelinga@gmail.com>
2025-05-14 01:32:00 +0000

    get thumbnail display working!

--- app/src/app/projects/[project]/page.tsx
+++ app/src/app/projects/[project]/page.tsx
@@ -42,7 +42,7 @@ export default function ProjectPage({ params }: { params: Promise<{ project: str
                     {item.discFiles.map((discFile) => (
                         <div key={discFile.file}>
                             <div>File: {discFile.file}</div>
-                            <div>Thumb: {discFile.thumb}</div>
+                            <img src={`http://localhost:25004/thumbs/${project}/${item.disc}/${discFile.thumb}`} alt="Thumbnail" />
                         </div>
                     ))}
                 </div>
