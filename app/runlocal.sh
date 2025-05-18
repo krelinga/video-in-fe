dd65e4e505ef5b08aa3c9eea3d7459f693bdba00
Andy Kreling <krelinga@gmail.com>
2025-05-18 06:36:41 +0000

    move img url prefix into an environment variable.
    add a runlocal.sh to pass values for these variables.

---
+++ app/runlocal.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+set -e
+
+export NEXT_PUBLIC_IMG_URL_PREFIX="http://localhost:25004"
+npm run dev
--- app/src/app/projects/[project]/page.tsx
+++ app/src/app/projects/[project]/page.tsx
@@ -4,6 +4,6 @@ import FileCategorize from '@/app/ui/fileCategorize'
 export default function ProjectPage({ params }: { params: Promise<{ project: string }> }) {
     const { project } = use(params);
     return (
-        <FileCategorize project={project} imgUrlPrefix={"http://localhost:25004"}/>
+        <FileCategorize project={project} imgUrlPrefix={process.env.NEXT_PUBLIC_IMG_URL_PREFIX!}/>
     )
 };
