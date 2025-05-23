5c1f3eb7cb85e2c77baaca9432f08f9eb43020f9
Andy Kreling <krelinga@gmail.com>
2025-05-23 00:00:33 +0000

    point to urls and backends that work on dev server.

--- runDockerLocal.sh
+++ runDockerLocal.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
 set -e
 
-export NEXT_PUBLIC_IMG_URL_PREFIX="http://localhost:25004"
-export NEXT_PUBLIC_BACKEND_URL="http://host.docker.internal:25004"
+export NEXT_PUBLIC_IMG_URL_PREFIX="https://video-in-be.dev.i.krel.ing"
+export NEXT_PUBLIC_BACKEND_URL="http://172.17.0.1:25004"
 docker build -t video-in-fe .
 docker run --rm \
     --env NEXT_PUBLIC_IMG_URL_PREFIX \
